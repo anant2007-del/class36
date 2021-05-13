79c07ff541cf61f6fa6f02ce7926121de8ac3e4f
anant2007-del <83063988+anant2007-del@users.noreply.github.com>
2021-05-13 15:14:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState = 0;
+var playerCount;
+var form,player,game;
+var database;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(400,400);
+    game=  new Game()
+    game.getState()
+    game.start()
+}
+
+function draw(){
+   
+}
